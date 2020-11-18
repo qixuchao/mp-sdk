3698c5ffe3fc26f6f6dcf3f131171e54d151b728
qixuchao <qixuchaovip@163.com>
2020-11-18 16:56:42 +0800

    update; 代码调整

--- src/union/vendor/gdt/GdtManager.js
+++ src/union/vendor/gdt/GdtManager.js
@@ -153,7 +153,6 @@ class GdtManager {
       const union = getUnionInstance(traceid);
       if (union) {
         union.onClick();
-        console.log(event);
         doClick.apply(this, arguments);
       }
     };
